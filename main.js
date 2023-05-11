436d3b7be7bdeab18d889e54be8cc83c5a3e50e1
sca <posta.email3001@gmail.com>
2023-05-12 01:15:09 +0200

    implementata colorazione delle celle adiacenti alle bombe

--- main.js
+++ main.js
@@ -9,144 +9,154 @@ const difficulty = document.getElementById("difficulty");
 const score = document.getElementById("score");
 const playButton = document.getElementById("play");
 
+const bombsNumber = 16;
 
 //genera griglia al click del pulsante play
 playButton.addEventListener("click", play);
 
-
-
-
 //FUNZIONI
 
 //numero random min max
-function randomNumMinMax (min, max){
-    return Math.floor(Math.random() * (max - min) + min);
+function randomNumMinMax(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 //crea array di tot(es.16) numeri scegliendoli randomicamente da un totale di tot(es.100) numeri senza ripeterli
-function arrRandomNumMinMax (howMany, numMin, numMax) {
-    const array = [];
+function arrRandomNumMinMax(howMany, numMin, numMax) {
+  const array = [];
 
-    while(array.length < howMany) {
-        const numero = randomNumMinMax(numMin, numMax);
-        
-        if (!array.includes(numero)) {
-            array.push(numero);
-        }
+  while (array.length < howMany) {
+    const numero = randomNumMinMax(numMin, numMax);
+
+    if (!array.includes(numero)) {
+      array.push(numero);
     }
-    
-    return array;
-    
+  }
+
+  return array;
 }
 
 //crea nuovo livello
-function newLevel (elements, difficulty) {
+function newLevel(totalCells, difficulty, verticalCellDistance) {
+  //istruzioni new game
+  console.clear();
+  let punteggio = 0;
 
-    //istruzioni new game
-    let punteggio = 0;
+  score.innerHTML = "SCORE: 00";
 
-    score.innerHTML = "SCORE: 00";
+  playButton.value = "New Game";
 
-    playButton.value = "New Game";
+  container.innerHTML = "";
 
-    container.innerHTML = "";
+  //crea array che contiene tutte le celle della griglia
+  const gridCells = document.getElementsByClassName("cell");
+  console.log(gridCells);
 
-    //crea array che contiene tutte le box della griglia
-    const gridBoxes = document.getElementsByClassName("box");
+  //crea array bombe
+  const bombe = arrRandomNumMinMax(16, 0, totalCells);
+  console.log(bombe);
 
-    //crea array bombe
-    const bombe = arrRandomNumMinMax (16, 1, elements);
+  //crea layover
+  const layover = document.createElement("div");
+  layover.classList.add("layover");
+  container.append(layover);
 
-    //crea layover
-    const layover = document.createElement("div");
-    layover.classList.add("layover");
-    container.append(layover);
+  //genera elementi (div) nella griglia
+  for (let i = 0; i < totalCells; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell", difficulty);
+    container.append(cell);
 
-    for (let i = 0; i < elements; i++) { //genera elementi (div) nella griglia
+    //flag per indicare se la cella è stata cliccata
+    let isClicked = false;
 
-        const box = document.createElement("div");
-        box.classList.add("box", difficulty);
-        container.append(box);
-        
-        //flag per indicare se la cella è stata cliccata
-        let isClicked = false;
+    //al click aggiungi classe che colora la cella di rosso se il n° di essa appartiene all'array bombe, se no di azzuro
+    cell.addEventListener(
+      "click",
 
-        //al click aggiungi classe che colora la cella di rosso se il n° di essa appartiene all'array bombe, se no di azzuro
-        box.addEventListener("click",
+      function () {
+        // se la cella è già stata cliccata, non fare nulla
+        if (isClicked) {
+          cell.classList.add("unclicked");
+          return;
+        } else {
+          isClicked = true; // setta il flag per indicare che la cella è stata cliccata
+        }
 
-            function(){
+        //se la cella diventa rossa è gameover, scopri tutte le celle, dai un alert e un layover per rendere le celle non cliccabili
+        if (bombe.includes(i)) {
+          //rende attivo il layover
+          layover.classList.add("active");
 
-                if (bombe.includes(i)) { //se la cella diventa rossa è gameover, scopri tutte le celle, dai un alert e un layover per rendere le celle non cliccabili
-
-                    //rende attivo il layover
-                    layover.classList.add("active");
-
-                    for (let i = 0; i < elements; i++) { //scopri tutte le celle
-
-                        if (bombe.includes(i)) {
-                            gridBoxes[i].classList.add("bomb")
-                        } else {
-                            gridBoxes[i].classList.add("clicked")
-                        }
-                        
-                    }
-
-                    //informa l'utente che ha perso
-                    setTimeout(() => {
-                        score.innerHTML = `GAME OVER   SCORE: 0${punteggio}`;
-                        alert(
-                        `GAME OVER   SCORE: 0${punteggio}`
-                        );
-
-                    }, 500);
-
-                } else { //se no la cella diventa azzurra e il punteggio viene incrementato   
-
-                    box.classList.add("clicked");
-
-                    if (isClicked) { // se la cella è già stata cliccata, non fare nulla
-                        box.classList.add("unclicked");
-                        return;
-                    }
-                    isClicked = true; // setta il flag per indicare che la cella è stata cliccata
-
-                    punteggio++;
-                    score.innerHTML = `SCORE: 0${punteggio}`;
-                    
-                    if (punteggio === elements - 16) { //se clicchi tutte le celle senza bombe hai vinto, scopri tutte le bombe
-                        for (let i = 0; i < elements; i++) {
-                            if (bombe.includes(i)) {
-                                gridBoxes[i].classList.add("bomb")
-                            } 
-                        }
-
-                        //attiva layover
-                        layover.classList.add("active");
-
-                        //informa l'utente che ha vinto
-                        setTimeout(() => {
-                            score.innerHTML = `YOU WIN   SCORE: 0${punteggio}`;
-                            alert(
-                            `YOU WIN   SCORE: 0${punteggio}`
-                            );
-
-                        }, 500);
-                    }
-                }    
+          //scopri tutte le celle
+          for (let i = 0; i < gridCells.length; i++) {
+            // se la cella corrente è una bomba, colora di rosso
+            if (bombe.includes(i)) {
+              gridCells[i].classList.add("bomb");
+            } else if (
+              //se la cella è adiacente alla bomba (destra sinistra sopra e sotto) colorala di arancione
+              bombe.includes(i - 1) ||
+              bombe.includes(i + 1) ||
+              bombe.includes(i - verticalCellDistance) ||
+              bombe.includes(i + verticalCellDistance)
+            ) {
+              gridCells[i].classList.add("close");
+            } else {
+              //se no colorala di azzurro
+              gridCells[i].classList.add("clicked");
             }
-        );
-    }
+          }
+
+          //informa l'utente che ha perso
+          setTimeout(() => {
+            score.innerHTML = `GAME OVER   SCORE: 0${punteggio}`;
+            alert(`GAME OVER   SCORE: 0${punteggio}`);
+          }, 500);
+        } else {
+          //se la cella è adiacente alla bomba (destra sinistra sopra e sotto) colorala di arancione e il punteggio viene incrementato
+          if (
+            bombe.includes(i - 1) ||
+            bombe.includes(i + 1) ||
+            bombe.includes(i - verticalCellDistance) ||
+            bombe.includes(i + verticalCellDistance)
+          ) {
+            cell.classList.add("close");
+          }
+          //se no la cella diventa azzurra e il punteggio viene incrementato
+          cell.classList.add("clicked");
+          punteggio++;
+          score.innerHTML = `SCORE: 0${punteggio}`;
+
+          //se clicchi tutte le celle senza bombe hai vinto, scopri tutte le bombe
+          if (punteggio === totalCells - bombsNumber) {
+            for (let i = 0; i < totalCells; i++) {
+              if (bombe.includes(i)) {
+                gridCells[i].classList.add("bomb");
+              }
+            }
+
+            //attiva layover
+            layover.classList.add("active");
+
+            //informa l'utente che ha vinto
+            setTimeout(() => {
+              score.innerHTML = `YOU WIN   SCORE: 0${punteggio}`;
+              alert(`YOU WIN   SCORE: 0${punteggio}`);
+            }, 500);
+          }
+        }
+      }
+    );
+  }
 }
 
 function play() {
-
-    //cambia le celle generate in base alla difficoltà
-    if (difficulty.value === "easy") {
-        newLevel(100, "easy");
-    } else if (difficulty.value === "medium") {
-        newLevel(81, "medium");
-    } else {
-        newLevel(49, "hard");
-    }
-
+  //cambia le celle generate in base alla difficoltà
+  if (difficulty.value === "easy") {
+    newLevel(100, "easy", 10);
+  } else if (difficulty.value === "medium") {
+    newLevel(81, "medium", 9);
+  } else {
+    newLevel(49, "hard", 7);
+  }
 }
